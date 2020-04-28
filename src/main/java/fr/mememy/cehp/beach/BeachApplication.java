72f60bc8442531d6860246d36dc06a1610b652ba
Geremy <ggeremy@gmail.com>
2020-04-28 19:24:37 +0200

    sprint initializr

---
+++ src/main/java/fr/mememy/cehp/beach/BeachApplication.java
@@ -0,0 +1,13 @@
+package fr.mememy.cehp.beach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/mememy/cehp/beach/BeachApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mememy.cehp.beach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
